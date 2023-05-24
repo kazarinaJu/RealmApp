059987391137e385945f5a968c43db3148e67ebc
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-05-24 19:11:40 +0500

    storage manager for tasks

--- RealmApp/Services/StorageManager.swift
+++ RealmApp/Services/StorageManager.swift
@@ -65,6 +65,25 @@ final class StorageManager {
         }
     }
     
+    func delete(_ task: Task) {
+        write {
+            realm.delete(task)
+        }
+    }
+    
+    func edit(_ task: Task, newTitle: String, newNote: String) {
+        write {
+            task.title = newTitle
+            task.note = newNote
+        }
+    }
+    
+    func done(_ task: Task) {
+        write {
+            task.isComplete.toggle()
+        }
+    }
+    
     private func write(completion: () -> Void) {
         do {
             try realm.write {
